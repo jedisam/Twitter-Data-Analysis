da35fd656542b7e416b8f45900a65c4e4190bd6a
Yididya <yidisam18@gmail.com>
2022-04-25 22:03:55 +0300

    adds unit test for extract_dataframe

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -8,6 +8,7 @@ from extract_dataframe import TweetDfExtractor, read_json
 sys.path.append(os.path.abspath(os.path.join('../..')))
 
 
+# _, tweet_list = read_json("data/Economic_Twitter_Data.json")
 _, tweet_list = read_json("data/Economic_Twitter_Data.json")
 
 columns = ['created_at', 'source', 'original_text', 'clean_text', 'sentiment', 'polarity', 'subjectivity', 'lang', 'favorite_count', 'retweet_count',
@@ -38,74 +39,73 @@ class TestTweetDfExtractor(unittest.TestCase):
 
     def test_find_full_text(self):
         """Test find full text method."""
-        text = ['RT @nikitheblogger: Irre: Annalena Baerbock sagt, es bricht ihr das Herz, dass man nicht bedingungslos schwere Waffen liefert.', 'Mir bricht e…', 'RT @sagt_mit: Merkel schaffte es in 1 Jahr 1 Million "Flüchtlinge" durchzufüttern, jedoch nicht nach 16 Jahren 1 Million Rentner aus der Ar…', 'RT @Kryptonoun: @WRi007 Pharma in Lebensmitteln, Trinkwasser, in der Luft oder in der Zahnpasta irgendwo muss ein Beruhigungsmittel bzw. Be…', ''
-
-
-
-                "Thank you @research2note for creating this amazing campaign &amp; turning social media #red4research today. @NHSRDFORUM is all about sharing the talent, passion  &amp; commitment of individuals coming together as a community for the benefit of all. You've done this. Well done 👋", 'Former Pfizer VP and Virologist, Dr. Michael Yeadon, is one of the most credentialed medical professionals speaking out about the dangers of the #Covid19 vaccines, breaks down his “list of lies” that keeps him up at night. https://t.co/LSE8CrKdqn', 'I think it’s important that we don’t sell COVAX short. It still has a lot going for it and is innovative in its design. But it needs more vaccines to share.  We’re hoping our low cost @TexasChildrens recombinant protein COVID19 vaccine with @biological_e will help fill some gaps']
+        text = ['RT @nikitheblogger: Irre: Annalena Baerbock sagt, es bricht ihr das Herz, dass man nicht bedingungslos schwere Waffen liefert.\nMir bricht e…', 'RT @sagt_mit: Merkel schaffte es in 1 Jahr 1 Million "Flüchtlinge" durchzufüttern, jedoch nicht nach 16 Jahren 1 Million Rentner aus der Ar…',
+                'RT @Kryptonoun: @WRi007 Pharma in Lebensmitteln, Trinkwasser, in der Luft oder in der Zahnpasta irgendwo muss ein Beruhigungsmittel bzw. Be…', 'RT @WRi007: Die #Deutschen sind ein braves Volk!. Mit #Spritpreisen von 2 Euro abgefunden. Mit #inflation abgefunden. Mit höheren #Abgaben…', 'RT @RolandTichy: Baerbock verkündet mal so nebenhin in Riga das Ende der Energieimporte aus Russland. Habeck rudert schon zurück, Scholz sc…']
 
         self.assertEqual(self.df.find_full_text(), text)
 
     def test_find_sentiments(self):
         """Test find sentiment module."""
-        self.assertEqual(self.df.find_sentiments(self.df.find_full_text()), ([0.16666666666666666, 0.13333333333333333, 0.3166666666666667, 0.08611111111111111, 0.27999999999999997], [
-                         0.18888888888888888, 0.45555555555555555, 0.48333333333333334, 0.19722222222222224, 0.6199999999999999]))
+        self.assertEqual(self.df.find_sentiments(self.df.find_full_text()), ([
+                         0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0]))
 
     def test_find_created_time(self):
         """Test find created time module."""
-        created_at = ['Fri Jun 18 17:55:49 +0000 2021', 'Fri Jun 18 17:55:59 +0000 2021', 'Fri Jun 18 17:56:07 +0000 2021',
-                      'Fri Jun 18 17:56:10 +0000 2021', 'Fri Jun 18 17:56:20 +0000 2021']
+        created_at = ['Fri Apr 22 22:20:18 +0000 2022', 'Fri Apr 22 22:19:16 +0000 2022',
+                      'Fri Apr 22 22:17:28 +0000 2022', 'Fri Apr 22 22:17:20 +0000 2022', 'Fri Apr 22 22:13:15 +0000 2022']
 
         self.assertEqual(self.df.find_created_time(), created_at)
 
     def test_find_source(self):
         """Test find source module."""
-        source = ['<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>', '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
-                  '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>', '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
-                  '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>']
+        source = ['<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>', '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+                  '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>', '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>', '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>']
 
         self.assertEqual(self.df.find_source(), source)
 
     def test_find_screen_name(self):
         """Test find screen name module."""
-        name = ['ketuesriche', 'Grid1949',
-                'LeeTomlinson8', 'RIPNY08', 'pash22']
-        self.assertEqual(self.df.find_screen_name(), name)
+        screen_name = ['McMc74078966', 'McMc74078966',
+                       'McMc74078966', 'McMc74078966', 'McMc74078966']
+        self.assertEqual(self.df.find_screen_name(), screen_name)
 
     def test_find_followers_count(self):
         """Test find follower count module."""
-        f_count = [551, 66, 1195, 2666, 28250]
+        f_count = [3, 3, 3, 3, 3]
         self.assertEqual(self.df.find_followers_count(), f_count)
 
     def test_find_friends_count(self):
         """Test find friend count module."""
-        friends_count = [351, 92, 1176, 2704, 30819]
+        friends_count = [3, 3, 3, 3, 3]
         self.assertEqual(self.df.find_friends_count(), friends_count)
 
     def test_find_is_sensitive(self):
         """Test find is sensetive module."""
         self.assertEqual(self.df.is_sensitive(), [
-                         None, None, None, None, None])
+                         '', '', '', '', ''])
 
     def test_find_favourite_count(self):
         """Test find favourite count module."""
         self.assertEqual(self.df.find_favourite_count(),
-                         [548, 195, 2, 1580, 72])
+                         [2356, 1985, 16, 1242, 1329])
 
     def test_find_retweet_count(self):
         """Test find retweet count module."""
-        self.assertEqual(self.df.find_retweet_count(), [612, 92, 1, 899, 20])
+        self.assertEqual(self.df.find_retweet_count(), [355, 505, 4, 332, 386])
 
-    # def test_find_hashtags(self):
-    #     self.assertEqual(self.df.find_hashtags(), )
+    def test_find_hashtags(self):
+        """_summary_statistics_test_find_hashtags."""
+        hashtags = [[], [], [], [{'text': 'Deutschen', 'indices': [16, 26]}, {'text': 'Spritpreisen', 'indices': [
+            54, 67]}, {'text': 'inflation', 'indices': [95, 105]}, {'text': 'Abgaben', 'indices': [130, 138]}], []]
+        self.assertEqual(self.df.find_hashtags(), hashtags)
 
     # def test_find_mentions(self):
     #     self.assertEqual(self.df.find_mentions(), )
 
-    def test_find_location(self):
-        """Test find location module."""
-        self.assertEqual(self.df.find_location(), [
-                         'Mass', 'Edinburgh, Scotland', None, None, 'United Kingdom'])
+    # def test_find_location(self):
+    #     """Test find location module."""
+    #     self.assertEqual(self.df.find_location(), [
+    #                      'Mass', 'Edinburgh, Scotland', None, None, 'United Kingdom'])
 
 
 if __name__ == '__main__':
