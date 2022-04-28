f75ae42b01d8b0568e5bc506cb87e44bf7720aff
Yididya <yidisam18@gmail.com>
2022-04-29 01:11:55 +0300

    adds logger

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,14 @@
+import sys
 from re import sub
 
 import nltk
-import seaborn as sns
 import pandas as pd
+import seaborn as sns
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import TweetTokenizer
+
+from logger import Logger
 
 nltk.download('wordnet')
 nltk.download('stopwords')
@@ -20,7 +23,15 @@ class Preprocessing:
 
     def __init__(self, df) -> None:
         """Initilize df."""
-        self.df = df
+        try:
+            self.df = df
+            self.logger = Logger("preprocessing.log").get_app_logger()
+            self.logger.info(
+                'Successfully Instantiated Preprocessing Class Object')
+        except Exception:
+            self.logger.exception(
+                'Failed to Instantiate Preprocessing Class Object')
+            sys.exit(1)
 
     def removePunc(self, myWord):
         """Remove punctuation from string inputs."""
@@ -57,24 +68,40 @@ class Preprocessing:
 
     def prepText(self, myWord):
         """Call other text pre-processing function."""
-        return self.removeStopWords(
-            self.lemmatize(
-                self.removeAscii(
-                    self.removePunc(
-                        self.removeLinkUser(
-                            self.myWord.lower()
+        try:
+            prep = self.removeStopWords(
+                self.lemmatize(
+                    self.removeAscii(
+                        self.removePunc(
+                            self.removeLinkUser(
+                                myWord.lower()
+                            )
                         )
                     )
                 )
             )
-        )
+            self.logger.info(
+                'Preprocessed text. Called: removePunc, removeAscii, lemmatize, removeLinkUser methods')
+            return prep
+        except Exception:
+            self.logger.exception(
+                'Fails to remove stop words, lemmatize, and clean all tweets')
+            sys.exit(1)
 
     def filterTweetList(self, tweetList):
         """Remove stop words, lemmatize, and clean all tweets."""
-        return [[self.prepText(word) for word
-                in tweet.split()
-                if self.prepText(word) is not None]
-                for tweet in tweetList]
+        try:
+            self.logger.info(
+                'Remove stop words, lemmatize, and clean all tweets')
+            return [[self.prepText(word) for word
+                     in tweet.split()
+                     if self.prepText(word) is not None]
+                    for tweet in tweetList]
+        except Exception:
+            self.logger.exception(
+                'Fails to filter stop words, lemmatize, and clean all tweets')
+            sys.exit(1)
+
     # def preprocess(self, df):
     #     """Preprocess a data frame."""
     #     df['text'] = df['text'].apply(self.prepText)
@@ -90,17 +117,19 @@ class Preprocessing:
         Returns:
             df: Dataframe with new column
         """
-        self.df[org_col] = self.df['org_col'].astype(str)
-        self.df[new_col] = self.df['original_text'].str.replace(
-            '[^\w\s]', '')
-        self.df[new_col] = self.df[new_col].str.replace(
-            '\w*\d\w*', '')
-        self.df[new_col] = self.df[new_col].apply(
-            lambda x: " ".join(x.lower() for x in x.split()))
-        self.df[new_col] = self.df[new_col].apply(
-            lambda x: " ".join(x for x in x.split() if x not in stop))
-
-        return self.df
+        try:
+            self.df[org_col] = self.df[org_col].astype(str)
+            self.df[new_col] = self.df[org_col].str.replace('[^\w\s]', '')
+            self.df[new_col] = self.df[new_col].str.replace('\w*\d\w*', '')
+            self.df[new_col] = self.df[new_col].apply(
+                lambda x: " ".join(x.lower() for x in x.split()))
+            self.df[new_col] = self.df[new_col].apply(
+                lambda x: " ".join(x for x in x.split() if x not in stop))
+            self.logger.info('cleans text')
+            return self.df
+        except Exception:
+            self.logger.exception('Fails to clean text')
+            sys.exit(1)
 
     def stem(self, col):
         """Stemm a word.
@@ -111,7 +140,12 @@ class Preprocessing:
         Returns:
             df: Dataframe with new column
         """
-        # tokenize each tweet to its root word
-        self.df[col] = self.df[col].apply(
-            lambda x: " ".join([stemmer.stem(word) for word in x.split()]))
-        return self.df
+        try:
+            # tokenize each tweet to its root word
+            self.df[col] = self.df[col].apply(
+                lambda x: " ".join([stemmer.stem(word) for word in x.split()]))
+            self.logger.info('tokenizes tweet')
+            return self.df
+        except Exception:
+            self.logger.exception('Fails to tokenize tweet')
+            sys.exit(1)
